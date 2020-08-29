92c947b264cff315a17601da52c0503ebb107bfb
Sam Barros <samarony.barros@gmail.com>
2020-08-29 12:34:14 +0200

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-// export { LinkedList } from './core/linked-list'
-import { SortedList } from './core/sorted-list'
-
-const list = new SortedList()
-const arr = [4, 7, 5, 1, 6, 2, 4, 3, 8]
-
-arr.forEach((el) => list.add(el))
-
-list.removeAllElementsFromList(5)
-
-list.print()
+export { LinkedList } from './core/linked-list'
+export { SortedList } from './core/sorted-list'
