195134c15b05bb497e46e907735527e260d92ddf
Sam Barros <samarony.barros@gmail.com>
2020-10-30 11:02:58 +0100

    v1.0.8 (#11)

--- src/core/hash-table.ts
+++ src/core/hash-table.ts
@@ -10,7 +10,20 @@ export class HashTable<T> {
     this.size = size
   }
 
-  public hash = (key: string) => key.toString().length % this.size
+  public hash = (key: string) => {
+    let hash = 0
+    if (key.length == 0) {
+      return hash
+    }
+
+    for (var i = 0; i < key.length; i++) {
+      var char = key.charCodeAt(i)
+      hash = (hash << 5) - hash + char
+      hash = hash & hash // Convert to 32bit integer
+    }
+
+    return hash % this.size
+  }
 
   public insert = (key: string, value: T) => {
     const index = this.hash(key)
